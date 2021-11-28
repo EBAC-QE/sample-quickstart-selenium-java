dd79df62785320809eae52b1bed0e3d27cb6c344
Ernesto Barbosa <ernestosbarbosa@outlook.com>
2021-11-28 17:24:15 -0300

    sample-quickstart-selenium-java

---
+++ src/test/java/LojaEbacTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LojaEbacTest {
+
+    static ChromeDriver driver;
+
+    @BeforeTest
+    public static void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void shouldAccessHomePage(){
+        driver.get("http://lojaebac.ebaconline.art.br/");
+        Assert.assertEquals(driver.getTitle(), "EBAC – Shop – Página de teste");
+    }
+
+    @AfterTest
+    public static void tearDown(){
+        driver.close();
+    }
+
+}
